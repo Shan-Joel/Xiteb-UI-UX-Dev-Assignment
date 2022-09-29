0293e03853f49bbb27fb62fabe2685d05aa2e60a
Shan Joel <shan.joeld@gmail.com>
2022-09-30 00:14:50 +0530

    finished upto homepage slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 const check = document.querySelector('.circle');
 const icon = document.querySelector('.fa-check');
+const dropdown = document.querySelector('.destination-dropdown');
+const otherDropdownValues = document.querySelector('.rest-of-the-options');
+const otherSelectedValue = document.querySelectorAll('.other-selected-value');
+const valueTxt = document.querySelector('.selected-value');
 
 check.addEventListener('click', () => {
    icon.classList.toggle('display-check');
@@ -8,3 +12,13 @@ check.addEventListener('click', () => {
 icon.addEventListener('click', () => {
    icon.classList.toggle('display-check');
 });
+
+dropdown.addEventListener('click', () => {
+   otherDropdownValues.classList.toggle('dropdown-display-block');
+   otherDropdownValues.style.transform = 'translateY(10px)';
+});
+
+function show(userSelectedOption) {
+   valueTxt.value = userSelectedOption;
+   otherDropdownValues.classList.remove('dropdown-display-block');
+}
